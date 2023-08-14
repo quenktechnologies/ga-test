7f3a19768e24608129f95b08ad73acd9b66cdb8a
Lasana Murray <lmurray@quenk.com>
2023-08-14 13:05:57 -0400

    test reporters

---
+++ test/main_test.js
@@ -0,0 +1,19 @@
+
+describe('main', () => {
+
+  it('should work', () => {
+    
+  });
+
+  it('should work good', () => {
+    
+  });
+
+  it('should fail ', () => {
+
+    throw new Error('fail != working');
+    
+  });
+  
+});
+
